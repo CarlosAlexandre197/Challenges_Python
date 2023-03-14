78cd0246904e497ecec0ae7fbe70688238e480b1
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-03-14 02:07:21 -0300

    Add files via upload
    Adição de arquivos.

---
+++ Soma_multiplicao_dois_numeros.py
@@ -0,0 +1,7 @@
+'''Faça um programa que leia dois números e faça a soma e a multiplicação entre eles.'''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+resultado_soma = n1 + n2
+resultado_multiplicacao = n1 * n2 
+print('A soma é: {} e a multiplicação é: {}'.format(resultado_soma, resultado_multiplicacao))
