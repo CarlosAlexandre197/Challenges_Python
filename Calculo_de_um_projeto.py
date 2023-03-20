385985c33fc46a5c48378d154b7a52d97cc17363
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-03-20 01:14:20 -0300

    Update Calculo_de_um_projeto.py
    Atualização do código.

--- Calculo_de_um_projeto.py
+++ Calculo_de_um_projeto.py
@@ -13,3 +13,18 @@ taxa = 0.15
 valor_total = (valor_inicial + horas_estimadas * valor_hora) * (1 + taxa)
 
 print(f'{valor_total:.2f}')
+
+
+#Exemplo02:
+
+horas_estimadas = 80
+valor_inicial = 1000.00
+valor_hora = 20.45
+taxa = 395.40 
+
+valor_total = (valor_inicial + horas_estimadas * valor_hora) + taxa
+
+print(f'{valor_total:.2f}')
+
+
+  
